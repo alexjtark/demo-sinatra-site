8d1d59e02da6a0fe27781dd94bd4b39fd23dab99
alexjtark <alex.tarkowski@gmail.com>
2015-01-10 09:06:05 -0500

    initial commit

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -2,10 +2,26 @@ get '/' do
   erb :welcome
 end
 
+get '/hello' do
+  erb :hello
+end
+
+# get '/name' do
+#   @name = params[:name]
+#   make view file named 'name', pass in a symbol @name
+# end
+
 get '/login' do
-  
+
 end
 
 get '/signup' do
-  
+
 end
+
+get '/data' do
+  erb :data
+end
+
+
+#  client = SODA::Client.new({:domain => "explore.data.gov", :app_token => "hnb8iYk1gGyTK5TCce4jWhtOw"})
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,28 @@
 $(document).ready(function() {
+
+  $('.welcome').mouseover(function (){
+    $('.welcome').css('color','red').fadeTo('slow', 0.5);
+  }).mouseout(function(){
+    $('.welcome').css('color', 'white').fadeTo('slow', 1);
+  });
+
+  $('.funny').mouseover(function(){
+    $('.funny').css('color', 'red').fadeTo('slow', 0.8);
+  }).mouseleave(function(){
+    $('.funny').css('color', 'black').fadeTo('slow', 1);
+  })
+
+
   // This is called after the document has loaded in its entirety
   // This guarantees that any elements we bind to will exist on the page
   // when we try to bind to them
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+
 });
+
+
+// .toggle({ effect: "scale", direction: "horizontal" });
+// });
+
+
